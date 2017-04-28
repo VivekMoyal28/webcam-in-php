07bbbfb0a02ea628d7ef9de61b192d6a29ed08f3
VivekMoyal28 <vivek@simplyitsols.com>
2017-04-28 16:30:49 +0530

    Update connectionClass.php

--- connectionClass.php
+++ connectionClass.php
@@ -1,13 +1,12 @@
 <?php
-class connectionClass {
-    private $host="localhost",$dbname="test",$dbpass="",$dbuser="root";
+class connectionClass extends mysqli{
+    public $host="localhost",$dbname="webcam",$dbpass="",$dbuser="root";
     public $con;
     
     public function __construct() {
-        $this->con=  mysqli_connect($this->host, $this->dbuser, $this->dbpass, $this->dbname);
-        if($this->con){}
-        else{
-//            echo "<h1>Error while connecting to database</h1>";
+        $this->con= $this->connect($this->host, $this->dbuser, $this->dbpass, $this->dbname);
+        if(!$this->con){
+            echo "<h1>Error while connecting to database</h1>";
         }
-    }
+        }
 }
